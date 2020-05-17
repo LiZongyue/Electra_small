cc9d78f4af14ca5c084a079636cd4d9bc17d57f9
ZongyueLi <zongyue.li@outlook.com>
2020-05-18 00:32:08 +0200

    New version release

--- train_from_scratch/setup.py
+++ train_from_scratch/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
    name='Electra',
-   version='2.0',
-   description='Train a smaller Electra from scratch and fine tune it with GLUE',
-   author='Zongyue Li',
+   version='2.1',
+   description='Train a smaller Electra from scratch and fine tune it with GLUE task SST',
+   author='Yifu Chen and Zongyue Li',
    author_email='zongyue.li@outlook.com',
    package_dir={"": "src"},
    packages=find_packages("src"),

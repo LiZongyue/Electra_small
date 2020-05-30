38b6a1adbaa518901f578aef92392fb73f812982
ZongyueLi <zongyue.li@outlook.com>
2020-05-30 16:05:30 +0200

    add BertRunner

--- examples/test.py
+++ examples/test.py
@@ -1,2 +1,13 @@
-import pandas as pd
+import torch
+from transformers import AutoModelWithLMHead
 
+model = AutoModelWithLMHead.from_pretrained("bert-base-uncased")
+
+pre_trained_weights = torch.load(model, map_location=torch.device('cpu'))
+
+state_to_load = {}
+for name, param in pre_trained_weights.items():
+    if name.startswith('electra.'):
+        state_to_load[name[len('electra.'):]] = param
+    elif name.startswith('bert.'):
+        state_to_load[name[len('bert.'):]] = param

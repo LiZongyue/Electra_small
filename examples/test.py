c9270d577c3b4da4d279a53adcfabff2881183b3
ZongyueLi <zongyue.li@outlook.com>
2020-05-30 16:29:12 +0200

    ignore test

--- examples/test.py
+++ examples/test.py
@@ -3,7 +3,8 @@ from transformers import AutoModelWithLMHead
 
 model = AutoModelWithLMHead.from_pretrained("bert-base-uncased")
 
-pre_trained_weights = torch.load(model, map_location=torch.device('cpu'))
+pre_trained_weights = torch.load("C:/Users/Zongyue Li/Documents/Github/BNP/output/Bert9.p",
+                                 map_location=torch.device('cpu'))
 
 state_to_load = {}
 for name, param in pre_trained_weights.items():
@@ -11,3 +12,5 @@ for name, param in pre_trained_weights.items():
         state_to_load[name[len('electra.'):]] = param
     elif name.startswith('bert.'):
         state_to_load[name[len('bert.'):]] = param
+
+print(state_to_load)
